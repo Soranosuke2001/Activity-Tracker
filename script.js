637a4df757b5da56ddee144677d3718562e10f71
Sora <80365991+Soranosuke2001@users.noreply.github.com>
2023-09-08 14:31:45 -0700

    refactored code to OOP

--- script.js
+++ script.js
@@ -11,73 +11,103 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-let map, mapEvent;
+class App {
+  #map;
+  #mapEvent;
 
-if (navigator.geolocation) {
-  // Fetching user position
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // Fetching approximate latitude and longitude
-      const { latitude, longitude } = position.coords;
-      const coords = [latitude, longitude];
+  constructor() {
+    // On page load, run the _getPosition() function
+    this._getPosition();
 
-      // Displaying leaflet map on user's coordinates
-      map = L.map("map").setView(coords, 12);
-      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    // User submits a new workout
+    form.addEventListener("submit", this._newWorkout.bind(this));
 
-      map.on("click", function (e) {
-        // Setting the map event
-        mapEvent = e;
+    // Changing form inputs on workout option change
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
 
-        // Display the workout form
-        form.classList.remove("hidden");
+  // Fetch user location
+  _getPosition() {
+    if (navigator.geolocation) {
+      // Fetching user position
+      navigator.geolocation.getCurrentPosition(
+        // On Success
+        this._loadMap.bind(this),
 
-        // Automatically set the focus to the first input box
-        inputDistance.focus();
-      });
-    },
-    // If the user location could not be fetched
-    function () {
-      alert("Unable to retrieve your location.");
+        // On Error
+        function () {
+          alert("Unable to retrieve your location.");
+        }
+      );
     }
-  );
+  }
+
+  // Display the map
+  _loadMap(position) {
+    // Fetching approximate latitude and longitude
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    // Displaying leaflet map on user's coordinates
+    this.#map = L.map("map").setView(coords, 12);
+    L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // Display workout form
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  // Display the workout form for the user to fill
+  _showForm(e) {
+    // Setting the map event
+    this.#mapEvent = e;
+
+    // Display the workout form
+    form.classList.remove("hidden");
+
+    // Automatically set the focus to the first input box
+    inputDistance.focus();
+  }
+
+  // Change input field on option selection
+  _toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  // User submits a new workout
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Fetching the position where the user clicked
+    const { lat, lng } = this.#mapEvent.latlng;
+    const pinCoord = [lat, lng];
+
+    // Adding the popup marker to the place where the user clicked
+    L.marker(pinCoord)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Workout")
+      .openPopup();
+
+    // Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+      inputCadence.value =
+        "";
+  }
 }
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  // Fetching the position where the user clicked
-  const { lat, lng } = mapEvent.latlng;
-  const pinCoord = [lat, lng];
-
-  // Adding the popup marker to the place where the user clicked
-  L.marker(pinCoord)
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    .setPopupContent("Workout")
-    .openPopup();
-
-  // Clear input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputElevation.value =
-    inputCadence.value =
-      "";
-});
-
-// Changing form inputs on workout option change
-inputType.addEventListener("change", function () {
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+// Initializing the app class
+const app = new App();
